5c07b9f8cea4fb32d2ef6416609ab6e638b33e33
vega <68478732+tekkvega@users.noreply.github.com>
2026-02-20 17:56:16 +0000

    Testing Fri Feb 20 2026 17:56:16 GMT+0000 (Coordinated Universal Time)

--- chapter10/ex01/student/studentTypeImp.cpp
+++ chapter10/ex01/student/studentTypeImp.cpp
@@ -1,1 +1,102 @@
 // Write your code here
+#include <iostream>
+#include "studentType.h"
+
+using namespace std;
+
+// Default Constructor
+studentType::studentType()
+{
+    firstName = "";
+    lastName = "";
+    courseGrade = 'F';
+    testScore = 0;
+    programmingScore = 0;
+    GPA = 0.0;
+}
+
+// Parameterized Constructor
+studentType::studentType(string fName, string lName,
+                         char grade, int tScore,
+                         int pScore, double gpa)
+{
+    firstName = fName;
+    lastName = lName;
+    courseGrade = grade;
+    testScore = tScore;
+    programmingScore = pScore;
+    GPA = gpa;
+}
+
+// Set functions
+void studentType::setFirstName(string val)
+{
+    firstName = val;
+}
+
+void studentType::setLastName(string val)
+{
+    lastName = val;
+}
+
+void studentType::setGrade(char val)
+{
+    courseGrade = val;
+}
+
+void studentType::setTestScore(int val)
+{
+    testScore = val;
+}
+
+void studentType::setProgScore(int val)
+{
+    programmingScore = val;
+}
+
+void studentType::setGPA(double val)
+{
+    GPA = val;
+}
+
+// Get functions
+string studentType::getFirstName() const
+{
+    return firstName;
+}
+
+string studentType::getLastName() const
+{
+    return lastName;
+}
+
+char studentType::getGrade() const
+{
+    return courseGrade;
+}
+
+int studentType::getTestScore() const
+{
+    return testScore;
+}
+
+int studentType::getProgScore() const
+{
+    return programmingScore;
+}
+
+double studentType::getGPA() const
+{
+    return GPA;
+}
+
+// Print function
+void studentType::print() const
+{
+    cout << "Name: " << firstName << " " << lastName << endl;
+    cout << "Grade: " << courseGrade << endl;
+    cout << "Test score: " << testScore << endl;
+    cout << "Programming score: " << programmingScore << endl;
+    cout << "GPA: " << GPA << endl;
+    cout << "***************" << endl;
+}
