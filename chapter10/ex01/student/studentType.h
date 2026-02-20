6346e45e2a34fea9dd2e19c99628e2a043d2b231
vega <68478732+tekkvega@users.noreply.github.com>
2026-02-20 17:46:41 +0000

    Testing Fri Feb 20 2026 17:46:41 GMT+0000 (Coordinated Universal Time)

--- chapter10/ex01/student/studentType.h
+++ chapter10/ex01/student/studentType.h
@@ -1,1 +1,37 @@
 // Write your code here
+#ifndef H_STUDENTTYPE
+#define H_STUDENTTYPE
+
+#include <string>
+
+using namespace std;
+
+class studentType
+{
+public:
+    // Set functions
+    void setFirstName(string val);
+    void setLastName(string val);
+    void setGrade(char val);
+    void setTestScore(int val);
+    void setProgScore(int val);
+    void setGPA(double val);
+
+    // Get functions
+    string getFirstName() const;
+    string getLastName() const;
+    char getGrade() const;
+    int getTestScore() const;
+    int getProgScore() const;
+    double getGPA() const;
+
+private:
+    string firstName;
+    string lastName;
+    char courseGrade;
+    int testScore;
+    int programmingScore;
+    double GPA;
+};
+
+#endif
--- chapter10/ex01/student/studentTypeImp.cpp
+++ chapter10/ex01/student/studentTypeImp.cpp
@@ -1,1 +1,64 @@
 // Write your code here
+#include "studentType.h"
+
+// Set functions
+void studentType::setFirstName(string val)
+{
+    firstName = val;
+}
+
+void studentType::setLastName(string val)
+{
+    lastName = val;
+}
+
+void studentType::setGrade(char val)
+{
+    courseGrade = val;
+}
+
+void studentType::setTestScore(int val)
+{
+    testScore = val;
+}
+
+void studentType::setProgScore(int val)
+{
+    programmingScore = val;
+}
+
+void studentType::setGPA(double val)
+{
+    GPA = val;
+}
+
+// Get functions
+string studentType::getFirstName() const
+{
+    return firstName;
+}
+
+string studentType::getLastName() const
+{
+    return lastName;
+}
+
+char studentType::getGrade() const
+{
+    return courseGrade;
+}
+
+int studentType::getTestScore() const
+{
+    return testScore;
+}
+
+int studentType::getProgScore() const
+{
+    return programmingScore;
+}
+
+double studentType::getGPA() const
+{
+    return GPA;
+}
