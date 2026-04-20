4cc5dfbaca559fa3ac4b19f7990d8582758dff72
vega <68478732+tekkvega@users.noreply.github.com>
2026-04-20 23:24:51 +0000

    chapter12/ex01 Task2

--- chapter12/ex01/student/main.cpp
+++ chapter12/ex01/student/main.cpp
@@ -1,26 +1,37 @@
 #include <iostream>
-#include <cstring> 
-#include <cctype> 
-  
+#include <cstring>
+#include <cctype>
+
 using namespace std;
 
-int main()
-{
-    char str[81];
-
+int main() {
+    // We use a temporary buffer to capture the initial input
+    char temp[100]; 
     int len;
 
-    int i;
-
     cout << "Enter a string: ";
-    cin.get(str, 80);
+    cin.get(temp, 100);
+
+    len = strlen(temp);
+
+    // Task #02: Using dynamic array allocation with 'new'
+    char *str = new char[len + 1];
+
+    // Copy the contents from temp to our dynamic array
+    strcpy(str, temp);
+
     cout << endl;
     cout << "String in upper case letters is:" << endl;
 
-    len = strlen(str);
-    for (i = 0; i < len; i++)
+    // Task #01: Process varying lengths and print in uppercase
+    for (int i = 0; i < len; i++) {
         cout << static_cast<char>(toupper(str[i]));
+    }
+    
     cout << endl;
+
+    // Deallocate the memory
+    delete[] str;
 
     return 0;
 }
