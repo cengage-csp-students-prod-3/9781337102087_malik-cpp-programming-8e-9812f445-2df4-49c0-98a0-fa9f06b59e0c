22a00f2eba0bbd700789849447f3bcd0cba6459d
vega <68478732+tekkvega@users.noreply.github.com>
2026-04-24 02:25:07 +0000

    Reset Exercise Fri Apr 24 2026 02:25:07 GMT+0000 (Coordinated Universal Time)

--- chapter13/ex01/student/main.cpp
+++ chapter13/ex01/student/main.cpp
@@ -1,27 +1,8 @@
 #include <iostream>
-#include "rectangleType.h"
 
 using namespace std;
 
 int main() {
-    rectangleType yard1(10, 6);  // Area 60
-    rectangleType yard2(8, 4);   // Area 32
-    rectangleType yard3(5, 10);  // Area 50
-
-    cout << "Yard 1: " << yard1 << endl;
-    cout << "Yard 2: " << yard2 << endl;
-
-    if (yard1 == yard3)
-        cout << "Yard 1 and Yard 3 have the same area." << endl;
-
-    if (yard1 > yard2)
-        cout << "Yard 1 area is greater than Yard 2." << endl;
-
-    rectangleType yard4 = yard1 - yard2;
-    cout << "Result of Yard 1 - Yard 2: " << yard4 << endl;
-
-    ++yard4;
-    cout << "After incrementing Yard 4: " << yard4 << endl;
-
+    // Write your main here
     return 0;
 }
