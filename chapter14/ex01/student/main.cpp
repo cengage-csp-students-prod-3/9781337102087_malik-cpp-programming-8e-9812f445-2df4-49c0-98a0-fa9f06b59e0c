d73e7a239fa5f75895de7687e666385ed6435b94
vega <68478732+tekkvega@users.noreply.github.com>
2026-04-24 01:38:10 +0000

    chapter14/ex01 Task1

--- chapter14/ex01/student/main.cpp
+++ chapter14/ex01/student/main.cpp
@@ -6,36 +6,20 @@ using namespace std;
 int main() {
     double feet, inches;
 
-    while (true) {
-        try {
-            cout << "Enter feet: ";
-            if (!(cin >> feet)) {
-                throw runtime_error("invalid");
-            }
+    try {
+        cin >> feet >> inches;
 
-            cout << "Enter inches: ";
-            if (!(cin >> inches)) {
-                throw runtime_error("invalid");
-            }
-
-            if (feet < 0 || inches < 0) {
-                throw runtime_error("negative");
-            }
-
-            // conversion
-            double totalInches = feet * 12 + inches;
-            double centimeters = totalInches * 2.54;
-
-            cout << fixed << setprecision(2);
-            cout << "Equivalent length in centimeters: " << centimeters << endl;
-
-            break; // exit loop after valid input
+        if (!cin || feet < 0 || inches < 0) {
+            throw runtime_error("error");
         }
-        catch (...) {
-            cout << "A non positive number is entered" << endl;
-            cin.clear();
-            cin.ignore(1000, '\n');
-        }
+
+        double centimeters = ((feet * 12) + inches) * 2.54;
+
+        cout << fixed << setprecision(2);
+        cout << centimeters << endl;
+    }
+    catch (...) {
+        cout << "A non positive number is entered" << endl;
     }
 
     return 0;
