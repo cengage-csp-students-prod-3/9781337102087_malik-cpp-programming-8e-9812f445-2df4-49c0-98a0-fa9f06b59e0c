39be889e8dcade77573d87bc683146545f36c17c
vega <68478732+tekkvega@users.noreply.github.com>
2026-04-24 02:38:24 +0000

    chapter15/ex01 Task1

--- chapter15/ex01/student/main.cpp
+++ chapter15/ex01/student/main.cpp
@@ -1,8 +1,32 @@
 #include <iostream>
-
 using namespace std;
 
+void starPattern(int n) {
+    if (n == 0)
+        return;
+
+    // print n stars
+    for (int i = 0; i < n; i++)
+        cout << "*";
+    cout << endl;
+
+    // recursive call
+    starPattern(n - 1);
+
+    // print n stars again
+    for (int i = 0; i < n; i++)
+        cout << "*";
+    cout << endl;
+}
+
 int main() {
-    // Write your main here
+    int lines;
+
+    cout << "Enter number of lines: ";
+    cin >> lines;
+
+    if (lines >= 0)
+        starPattern(lines);
+
     return 0;
 }
